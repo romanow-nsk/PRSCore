9b9cf4ac07be7fc3ac0a0bfaad9a540d48aa7891
romanow-nsk <romanow@ngs.ru>
2022-05-13 01:11:42 +0700

    Прием экзамена

--- src/main/java/romanow/abc/core/entity/subjectarea/EMExamTaking.java
+++ src/main/java/romanow/abc/core/entity/subjectarea/EMExamTaking.java
@@ -9,7 +9,7 @@ import romanow.abc.core.utils.OwnDateTime;
 
 public class EMExamTaking extends EMNamedEntity{
     @Getter @Setter OwnDateTime startTime = new OwnDateTime(0);
-    @Getter @Setter OwnDateTime endTime = new OwnDateTime(0);
+    @Getter @Setter int duration=0;
     @Getter @Setter int state = Values.TakingUndefined;
     @Getter EntityLink<EMDiscipline> EMDiscipline = new EntityLink<>();                 // Обратная ссылка
     @Getter EntityLink<EMExam> exam = new EntityLink<>();                               // Обратная ссылка на экзамен
@@ -17,4 +17,7 @@ public class EMExamTaking extends EMNamedEntity{
     @Getter EntityLink<EMGroup> group = new EntityLink<>(EMGroup.class);                // Группа, для которой прием - прямые ссылки
     @Getter EntityLinkList<EMTicket> tickets = new EntityLinkList<>(EMTicket.class);    // Студенты, для которых прием - прямые ссылки
     public EMExamTaking(){}
+    public String getTitle(){
+        return  getName().length()==0 ? "..." : getName()+" "+getStartTime().dateTimeToString();
+        }
 }
