535ad91dc9b7ac0237f4d46dadd159e9a21ec959
romanow-nsk <romanow@ngs.ru>
2022-08-14 22:32:52 +0700

    Назначение рейтингов

--- src/main/java/romanow/abc/core/entity/subjectarea/SATeacher.java
+++ src/main/java/romanow/abc/core/entity/subjectarea/SATeacher.java
@@ -8,7 +8,7 @@ import romanow.abc.core.entity.EntityRefList;
 import romanow.abc.core.entity.users.User;
 
 public class SATeacher extends Entity {
-    @Getter private EntityLink<User> User = new EntityLink<>(User.class);
+    @Getter private EntityLink<User> user = new EntityLink<>(User.class);
     @Getter private EntityLinkList<SAGroupRating> ratings = new EntityLinkList<>(SAGroupRating.class);
     public SATeacher(){}
 }
