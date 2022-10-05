9158df480a441903b517a95b0f0b02e564e61222
romanow-nsk <romanow@ngs.ru>
2022-10-05 12:24:50 +0700

    Генерация котлин-каркаса

--- src/main/java/romanow/abc/core/PRSEnvironment.java
+++ src/main/java/romanow/abc/core/PRSEnvironment.java
@@ -1,12 +1,19 @@
 package romanow.abc.core;
 
+import romanow.abc.core.API.RestAPIBase;
 import romanow.abc.core.constants.Values;
 import romanow.abc.core.mongo.DAO;
+import romanow.abc.core.mongo.KotlinJSConverter;
+
+import java.util.Optional;
 
 public class PRSEnvironment {
     //------------------------------------------------------------------------------------------------------------------
     public static void main(String aa[]){
         Values.init();
-        DAO.createKotlinClassSources();
+        ErrorList errorList = new ErrorList();
+        KotlinJSConverter.createKotlinClassSources(errorList);
+        System.out.println(KotlinJSConverter.createJSAPIFace(RestAPIBase.class,errorList));
+        System.out.println(errorList.toString());
         }
     }
